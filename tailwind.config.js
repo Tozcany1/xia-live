d12a34bdf39c0183b3e2b3591d4c6b1d23193dd1
Tozcany1 <pipastolucamx@gmail.com>
2026-04-16 21:00:48 -0600

    Update tailwind.config.js
    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: "hsl(var(--primary))",
+        secondary: "hsl(var(--secondary))",
+        muted: "hsl(var(--muted))",
+        accent: "hsl(var(--accent))",
+        destructive: "hsl(var(--destructive))",
+      },
+    },
   },
   plugins: [],
 }
